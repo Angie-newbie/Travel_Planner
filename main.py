85c55ae2b0ef7ef57a9f190a311c1c571720d21a
Angie <angel890817@hotmail.com>
2025-03-21 12:22:58 +1100

    TryingNoen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from init import db, ma
-from models.users import User
 import os
+from dotenv import load_dotenv
 from blueprints.database_bp import db_bp
 from blueprints.users_bp import users_bp
 from blueprints.categories_bp import categories_bp
@@ -11,8 +11,10 @@ from blueprints.expenses_bp import expenses_bp
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lms_dev:123456@localhost:5432/travelplanner_db'
+    load_dotenv(override=True)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     ma.init_app(app)
